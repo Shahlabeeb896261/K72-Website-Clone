07edb918452046c478891c62bd499e9dbee47955
Shahlabeeb <shahlabeebsidd21@gmail.com>
2026-09-10 08:10:19 +0530

    update home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,27 @@
+import HomeTop from "../../components/Home/HomeTop";
+import HomeBottom from "../../components/HomeBottom/HomeBottom";
+import HomeText from "../../components/HomeText/HomeText";
+import Video from "../../components/Video/Video";
 import "./Home.css";
 
 const Home = () => {
   return (
-    <>Home</>
+    <>
+      <div className="home-container">
+        <div className="home-video">
+          <Video />
+        </div>
+        <div className="home-t">
+          <HomeTop />
+        </div>
+        <div className="home-b">
+          <HomeBottom />
+        </div>
+        <div className="home-tex">
+          <HomeText />
+        </div>
+      </div>
+    </>
   )
 }
 
