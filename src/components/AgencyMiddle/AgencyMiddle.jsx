cb37df57fff7722db7d7a1e923acd7929f94af48
Shahlabeeb <shahlabeebsidd21@gmail.com>
2026-09-10 08:08:06 +0530

    update agencymiddle component

---
+++ src/components/AgencyMiddle/AgencyMiddle.jsx
@@ -0,0 +1,35 @@
+import "./AgencyMiddle.css";
+
+const AgencyMiddle = () => {
+  return (
+    <div>
+                <div>
+          <div>
+            <h2>Expertise</h2>
+            <p>
+              Our Work_ Born in curiosity, raised by dedication and fed with a
+              steady diet of creativity.
+            </p>
+          </div>
+          <div>
+            <ul>
+              <li>Strategy</li>
+              <li>Advertising</li>
+              <li>Branding</li>
+              <li>Design</li>
+              <li>Content</li>
+            </ul>
+            <p>
+              Our Work_ Born in curiosity, raised by dedication and fed with a
+              steady diet of creativity.
+            </p>
+          </div>
+          <div>
+            <p>Our Culture_ We're open to each other. Period. The team works together to create a space that makes us proud.</p>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default AgencyMiddle
