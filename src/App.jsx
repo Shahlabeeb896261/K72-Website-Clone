d59f580a2a9e45d6012b98be029903d6fe7e5778
Shahlabeeb <shahlabeebsidd21@gmail.com>
2026-09-10 08:06:40 +0530

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home/Home";
 import "./App.css";
+import Agency from "./Pages/Agency/Agency";
+import Work from "./Pages/Work/Work";
 
 const App = () => {
   return (
@@ -6,6 +10,8 @@ const App = () => {
       <div className="container">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/agency" element={<Agency />} />
+          <Route path="/work" element={<Work />} />
         </Routes>
       </div>
     </>
