017ec0838288d4c83e579e949fbde4bc523c594f
Shahlabeeb <shahlabeebsidd21@gmail.com>
2026-09-10 08:09:22 +0530

    update home text component

---
+++ src/components/HomeText/HomeText.jsx
@@ -0,0 +1,13 @@
+import "./HomeText.css";
+
+const HomeText = () => {
+  return (
+    <>
+      <div className="homeText-container">
+        <p>K72 is an agency that builds brands from every angle. Today, tomorrow and years from now. We think the best sparks fly when comfort zones get left behind and friction infuses our strategies, brands and communications with real feeling. We're transparent, honest and say what we mean, and when we believe in something, we're all in.</p>
+      </div>
+    </>
+  )
+}
+
+export default HomeText;
