b8029f3aa4bcee057cf678e9f46e584eaac1c2c0
Shahlabeeb <shahlabeebsidd21@gmail.com>
2026-09-10 08:08:28 +0530

    update hometop component

---
+++ src/components/Home/HomeTop.jsx
@@ -0,0 +1,16 @@
+import Video from "../Video/Video";
+import "./HomeTop.css";
+
+const HomeTop = () => {
+  return (
+    <>
+      <div className="homeTop-container">
+        <div className="first">The spark for</div>
+        <div className="second">All<div><Video /></div>Things</div>
+        <div className="third">Creative</div>
+      </div>
+    </>
+  );
+};
+
+export default HomeTop;
