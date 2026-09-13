87484cf19911672223be9515e35a2b380daa9932
Shahlabeeb <shahlabeebsidd21@gmail.com>
2026-09-13 14:38:30 +0530

    update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import LineAnimation from "./components/LineAnimation/LineAnimation.jsx";
 import "./index.css";
 import App from "./App.jsx";
 
